a908b0fd33b29843fc17804b35ad9bbf6fffb6ce
vicentesousa <vicente.gppcom@gmail.com>
2019-09-23 16:32:31 -0300

    First version

---
+++ loraScript.py
@@ -0,0 +1,107 @@
+from collections import defaultdict
+import os
+import math
+import random
+import re
+import subprocess
+import sys
+import argparse
+from glob import glob
+import numpy as np
+import matplotlib.pylab as plt
+from itertools import cycle
+# To install YAML: sudo apt-get install python3-yaml
+import yaml
+
+class Simulation:
+    def __init__(self, configurations_file):
+        self.email_to = 'martinsdecastro23@gmail.com'            
+        with open(configurations_file, 'r') as f:
+            self.doc = yaml.load(f, Loader=yaml.loader.BaseLoader)
+        self.campaign_name = os.path.splitext(configurations_file)[0]
+        
+        # Read commom parameters
+        self.campaign = self.doc['scenario']['campaign']
+        self.simLocation = str(doc['scenario']['simLocation'])
+        self.simulationTime = self.doc['scenario']['simulationTime']
+        self.nDevices = self.doc['scenario']['nDevices'][0]
+        self.radius = self.doc['scenario']['radius'][0]
+        self.appPeriodSeconds = self.doc['scenario']['appPeriodSeconds']
+        self.bPrint = (self.doc['scenario']['bPrint'])
+        self.fixedSeed = (self.doc['scenario']['fixedSeed'])
+        self.algoritmo = self.doc['scenario']['Algoritmo']
+        self.ns3_path = str(self.doc['scenario']['ns3_path'])
+        self.ns3_path = os.getcwd() + '/' + self.ns3_path
+        self.ns3_script = str(self.doc['scenario']['ns3_script'])
+        self.nJobs = int(self.doc['scenario']['jobs'])
+        self.filename = str(self.doc['scenario']['filename'])
+       
+    def runCampaign(self,curCampaign):
+        # Configure simulation file in accordance with campaign parameter
+        sh_name = self.campaign_name + '_' + self.simLocation + '_' + curCampaign
+        print(curCampaign+" campaign written in file: " 'run_%s.sh' % sh_name)
+        with open('run_all_%s.sh' % sh_name, 'w') as f:                    
+            if self.simLocation == 'cluster':
+              print('To be implemented')
+            else:
+              f.write('#!/bin/bash\n')
+              f.write("cd '"+self.ns3_path+"'"+"\n")
+              outputDir = self.ns3_path+'/results_'+self.simLocation + '_' + curCampaign
+              f.write('rm -rf '+outputDir+' 2>/dev/null\n')
+              f.write('mkdir -p '+outputDir+'\n')
+            for iJob in range(0, self.nJobs):
+                jobRunSeed = random.randint(1, 23*10**14)
+                for iAlg in self.algoritmo:                           
+                    for varParam in self.doc['scenario'][curCampaign]:
+                      if str(curCampaign) == 'radius':
+                          command = (
+                          'NS_GLOBAL_VALUE="RngRun='+str(jobRunSeed)+ '" ' +
+                          "./waf --run '"+self.ns3_script+
+                          " --radius="+varParam+
+                          " --nDevices="+self.nDevices+
+                          " --simulationTime="+self.simulationTime+
+                          " --appPeriodSeconds="+self.appPeriodSeconds+
+                          " --print="+self.bPrint+
+                          " --fixedSeed="+str(self.fixedSeed)+
+                          " --algoritmo="+iAlg+
+                          " --filename="+ self.filename +
+                          " --outputDir='"+outputDir+"'"
+                          "'"
+                          )
+                      elif str(curCampaign) == 'nDevices':
+                          command = (
+                          'NS_GLOBAL_VALUE="RngRun='+str(jobRunSeed)+ '" ' +
+                          "./waf --run '"+self.ns3_script+ 
+                          " --radius="+self.radius+
+                          " --nDevices="+varParam+
+                          " --simulationTime="+self.simulationTime+
+                          " --appPeriodSeconds="+self.appPeriodSeconds+
+                          " --print="+self.bPrint+
+                          " --fixedSeed="+str(self.fixedSeed)+
+                          " --algoritmo="+iAlg+
+                          " --filename="+ self.filename +
+                          " --outputDir='"+outputDir+"'"
+                          "'"
+                          )
+                      f.write(command+' & wait\n')
+                      
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", type=str, help='Configuration File')
+args = parser.parse_args()
+
+configurations_file = args.file; 
+with open(configurations_file, 'r') as f:
+    doc = yaml.load(f, Loader=yaml.loader.BaseLoader)
+    campaign_name = os.path.splitext(configurations_file)[0]
+
+print('Simulação escolhida: ')
+campaign = doc['scenario']['campaign']
+print(campaign)
+                 
+simu = Simulation(configurations_file)
+
+for simC in campaign:
+    if str(simC) == 'nDevices' or str(simC) == 'radius':
+        simu.runCampaign(simC);
+    else:
+        print('Invalid simulation campaign: verify the campaign parameter!')
---
+++ plotScript.py
@@ -0,0 +1,221 @@
+# Legend
+#   RunSeed: 0
+#   Seed: 1
+#   Algoritmo: 2
+#   Radius: 3
+#   nDevices: 4
+#   appPeriodSeconds:5
+#   simulationTime:6
+#   PHYTotal: 7
+#   PHYSuccessful:8 
+#   PHYInterfered: 9
+#   PHYNoMoreReceivers: 10
+#   PHYUnderSensitivity: 11
+#   PHYLostBecauseTX: 12
+#   SimulationDuration: 13
+
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import matplotlib.pylab as plt
+from itertools import cycle
+import scipy.stats as st
+import os
+import argparse
+import yaml
+import matplotlib
+
+class openSimulation:
+    def __init__(self, configurations_file):
+        self.email_to = 'martinsdecastro23@gmail.com'            
+        with open(configurations_file, 'r') as f:
+            self.doc = yaml.load(f, Loader=yaml.loader.BaseLoader)
+        self.campaign_name = os.path.splitext(configurations_file)[0]
+        
+        # Read commom parameters
+        self.showPlot = True;
+        self.plotCI = True;                
+        self.campaign = self.doc['scenario']['campaign']
+        self.simLocation = str(doc['scenario']['simLocation'])
+        self.simulationTime = self.doc['scenario']['simulationTime']
+        self.nDevices = self.doc['scenario']['nDevices']
+        self.nDevices = list(map(float,self.nDevices))
+        self.radius = self.doc['scenario']['radius']
+        self.radius = list(map(float,self.radius))
+        self.appPeriodSeconds = self.doc['scenario']['appPeriodSeconds']
+        self.bPrint = (self.doc['scenario']['bPrint'])
+        self.fixedSeed = (self.doc['scenario']['fixedSeed'])
+        self.algoritmo = self.doc['scenario']['Algoritmo']
+        self.ns3_path = str(self.doc['scenario']['ns3_path'])
+        self.ns3_path = os.getcwd() + '/' + self.ns3_path
+        self.ns3_script = str(self.doc['scenario']['ns3_script'])
+        self.nJobs = int(self.doc['scenario']['jobs'])
+        if self.nJobs == 1: # disable the confidence interval
+            self.plotCI = False;
+        self.filename = str(self.doc['scenario']['filename'])
+        
+    def plotCampaign(self,curCampaign, metric):
+        # some general configurations
+        outputDir = self.ns3_path+'/results_'+self.simLocation + '_' + curCampaign        
+        chFile = outputDir+ "/" + self.filename + '.txt'        
+        print(chFile)
+        simTime = np.loadtxt(chFile, skiprows=1, usecols=(6, ), delimiter=',', unpack=False)                
+        resalg = np.loadtxt(chFile, skiprows=1, usecols=(2, ), delimiter=',', unpack=False)
+        resradius = np.loadtxt(chFile, skiprows=1, usecols=(3, ), delimiter=',', unpack=False)
+        resnDevices = np.loadtxt(chFile, skiprows=1, usecols=(4, ), delimiter=',', unpack=False)
+        ressimDur = np.loadtxt(chFile, skiprows=1, usecols=(13, ), delimiter=',', unpack=False)
+        resphyTotal = np.loadtxt(chFile, skiprows=1, usecols=(7, ), delimiter=',', unpack=False)
+        resphySucc = np.loadtxt(chFile, skiprows=1, usecols=(8, ), delimiter=',', unpack=False)
+            
+        markersA = cycle(('o', 'v', '^', 's', '>', 'p', '*', 'h', '<', 'H', 'D', 'd'))
+        colors = cycle(('b', 'g', 'r', 'c', 'm', 'y', 'k'))                    
+        plt.figure()     
+        for iAlg in self.algoritmo:
+            m_plr, m_plrCI, m_tput, m_tputCI, m_pkt, m_pktCI = [], [], [], [], [], []
+            for varParam in self.doc['scenario'][curCampaign]:                
+                if str(curCampaign) == 'radius':
+                    resalgIndexs = (resalg == int(iAlg)) & (resradius == int(varParam))
+                    xlabel='Distância [m]'
+                    resxData = self.radius; 
+                elif str(curCampaign) == 'nDevices':
+                    resalgIndexs = (resalg == int(iAlg)) & (resnDevices == int(varParam))                    
+                    xlabel='Quantidade de Dispositivos'
+                    resxData = self.nDevices;
+                # Plr Evaluation
+                label = "ADR_"+iAlg
+                color=next(colors)
+                marker=next(markersA)
+                if metric=='PLR':                
+                    # Calculate PLR line
+                    plr = 100*( resphyTotal[resalgIndexs]-resphySucc[resalgIndexs] ) / resphyTotal[resalgIndexs]
+                    m_plr = np.append(m_plr, np.mean(plr))
+                    # Confidence interval according to https://stackoverflow.com/questions/15033511/compute-a-confidence-interval-from-sample-data
+                    l, h = st.t.interval(0.95, len(plr)-1, loc=np.mean(plr), scale=st.sem(plr))
+                    #l,h = st.norm.interval(0.95, loc=np.mean(plr), scale=st.sem(plr))                                                            
+                    m_plrCI = np.append(m_plrCI, h)
+                elif metric=='Tput':
+                    # TODO
+                    # Calculate PLR line
+                    tput = 23*( resphySucc[resalgIndexs] ) / float(self.simulationTime)
+                    m_tput = np.append(m_tput, np.mean(tput))
+                    # Confidence interval according to https://stackoverflow.com/questions/15033511/compute-a-confidence-interval-from-sample-data
+                    l, h = st.t.interval(0.95, len(tput)-1, loc=np.mean(tput), scale=st.sem(tput))
+                    #l,h = st.norm.interval(0.95, loc=np.mean(tput), scale=st.sem(tput))                    
+                    m_tputCI = np.append(m_tputCI, h)
+                elif metric=='Pkt':
+                    # TODO
+                    # Calculate PLR line
+                    pkt = 100 * ( resphySucc[resalgIndexs] ) / resphyTotal[resalgIndexs]
+                    m_pkt = np.append(m_pkt, np.mean(pkt))
+                    # Confidence interval according to https://stackoverflow.com/questions/15033511/compute-a-confidence-interval-from-sample-data
+                    l, h = st.t.interval(0.95, len(pkt)-1, loc=np.mean(pkt), scale=st.sem(pkt))
+                    #l,h = st.norm.interval(0.95, loc=np.mean(pkt), scale=st.sem(pkt))                    
+                    m_pktCI = np.append(m_pktCI, h)
+
+                
+            # Plot line
+            if metric=='PLR':                         
+                plt.plot(resxData,m_plr, label=label, marker=marker,color=color)   
+                if bool(self.plotCI):
+                    plt.errorbar(resxData,m_plr, yerr=(m_plrCI-m_plr), marker=marker,color=color, ls = 'none', lw = 2, capthick = 2)
+            elif metric=='Tput':
+                # TODO
+                plt.plot(resxData,m_tput, label=label, marker=marker,color=color)    
+                if bool(self.plotCI):                
+                    plt.errorbar(resxData,m_tput, yerr = (m_tputCI-m_tput), marker=marker,color=color, ls = 'none', lw = 2, capthick = 2)
+            elif metric=='Pkt':
+                # TODO
+                plt.plot(resxData,m_pkt, label=label, marker=marker,color=color)    
+                if bool(self.plotCI):                
+                    plt.errorbar(resxData,m_pkt, yerr = (m_pktCI-m_pkt), marker=marker,color=color, ls = 'none', lw = 2, capthick = 2)
+            
+        # Plot figure
+        params = {'legend.fontsize': 'x-large',
+         'axes.labelsize': 'x-large',
+         'axes.titlesize':'x-large',
+         'xtick.labelsize':'x-large',
+         'ytick.labelsize':'x-large'}
+        plt.rcParams.update(params)        
+        os.makedirs(outputDir+"/ps", exist_ok=True)
+        os.makedirs(outputDir+"/png", exist_ok=True)
+        
+        if metric=='PLR':        
+            # Show and save PLR plot
+            ylabel="PLR [%]"
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            plt.legend(loc='best', numpoints=1)        
+            plt.grid()
+            plt.tight_layout()
+            if bool(self.plotCI):
+                imgfilename = 'PLR_CI_'+curCampaign
+            else:
+                imgfilename = 'PLR_'+curCampaign
+            plt.savefig(outputDir+"/png/"+imgfilename+".png")
+            plt.savefig(outputDir+"/ps/"+imgfilename+".eps")
+            if bool(self.showPlot):
+                plt.show()
+            else:
+                plt.close()
+        elif metric=='Tput':
+            # TODO
+            # Show and save PLR plot
+            ylabel="Tput [bps]"
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            plt.legend(loc='best', numpoints=1)        
+            plt.grid()
+            plt.tight_layout()
+            if bool(self.plotCI):
+                imgfilename = 'TPUT_CI_'+curCampaign
+            else:
+                imgfilename = 'TPUT_'+curCampaign    
+            plt.savefig(outputDir+"/png/"+imgfilename+".png")
+            plt.savefig(outputDir+"/ps/"+imgfilename+".eps")       
+            if bool(self.showPlot):
+                plt.show()
+            else:
+                plt.close()
+
+        elif metric=='Pkt':
+            # TODO
+            # Show and save PLR plot
+            ylabel="Pacotes recebidos [%]"
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            #plt.legend(loc='upper left', numpoints=1)        
+            plt.legend(loc='best', numpoints=1)        
+            plt.grid()
+            plt.tight_layout()
+            if bool(self.plotCI):
+                imgfilename = 'PKT_CI_'+curCampaign
+            else:
+                imgfilename = 'PKT_'+curCampaign    
+            plt.savefig(outputDir+"/png/"+imgfilename+".png")
+            plt.savefig(outputDir+"/ps/"+imgfilename+".eps")       
+            if bool(self.showPlot):
+                plt.show()
+            else:
+                plt.close()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", type=str, help='Configuration File')
+args = parser.parse_args()
+
+configurations_file = args.file; 
+with open(configurations_file, 'r') as f:
+    doc = yaml.load(f, Loader=yaml.loader.BaseLoader)
+    campaign_name = os.path.splitext(configurations_file)[0]
+
+# print(doc)
+print('Simulação escolhida: ')
+campaign = doc['scenario']['campaign']
+print(campaign)
+                 
+simu = openSimulation(configurations_file)
+for iMet in ['PLR', 'Tput', 'Pkt','Stime']:
+    for simC in campaign:
+        if str(simC) == 'nDevices' or str(simC) == 'radius':
+            simu.plotCampaign(simC,iMet);
+        else:
+            print('Invalid simulation campaign: verify the campaign parameter!')
