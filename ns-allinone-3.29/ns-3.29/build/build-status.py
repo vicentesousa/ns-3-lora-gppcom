cd0eda4cae4286cf64e62ad16c826e7442190271
vicentesousa <vicente.gppcom@gmail.com>
2019-10-06 10:17:13 -0300

    updates

--- ns-allinone-3.29/ns-3.29/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/subdir/ns3.29-subdir-debug', 'build/scratch/ns3.29-ADR_code-debug', 'build/scratch/ns3.29-scratch-simulator-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
